0ad7e18edae08fa3d0c772650c79d1c7148758aa
NadyaSianipar <85157263+NadyaSianipar@users.noreply.github.com>
2021-06-01 15:04:11 +0700

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,123 @@
+<?php
+require_once('config.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Registration | PHP</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+
+<div>
+	<?php
+	 	if(isset($_POST['create'])){
+
+	 		$firstname   = $_POST['firstname'];
+	 		$lastname    = $_POST['lastname'];
+	 		$email       = $_POST['email'];
+	 		$phonenumber = $_POST['phonenumber'];
+	 		$password    = $_POST['password'];
+
+	 		$sql = "INSERT INTO users (firstname, lastname, email, phonenumber, password ) VALUES(?,?,?,?,?)";
+	 		$stmtinsert = $db->prepare($sql);
+	 		$result = $stmtinsert->execute([$firstname, $lastname, $email, $phonenumber, $password]);
+	 		if($result){
+	 			echo "Successfully saved.";
+	 		}else{
+	 			echo 'There were errors while savving the data.';
+	 		}
+	 	}
+	?>
+</div>
+
+<div>
+	<form action="registration.php" method="post">
+		<div class="container"></div>
+
+			<div class="row">
+				<div class="col-sm-2">
+					<h1>Registration</h1>
+					<p>Fill up the form with correct values.</p>
+					<hr class="mb-3">
+					<label for="firstname"><b>First Name</b></label>
+					<input class="form-control" id="firstname" type="text" name="firstname" required>
+
+					<label for="lastname"><b>Last Name</b></label>
+					<input class="form-control" id="lastname" type="text" name="lastname" required>
+
+					<label for="email"><b>Email Address</b></label>
+					<input class="form-control" id="email" type="email" name="email" required>
+
+					<label for="phonenumber"><b>Phone</b></label>
+					<input class="form-control" id="phonenumber" type="text" name="phonenumber" required>
+
+					<label for="password"><b>Password</b></label>
+					<input class="form-control" id="password" type="password" name="password" required>
+					<hr class="mb-3">
+
+					<input class="btn btn-primary" type="submit" id="register" name="create" value="Sign Up">
+
+
+		</div>
+		</div>
+	</form>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script type="text/javascript">
+	$(function(){
+		$('#register').click(function(e){
+
+			var valid = this.form.checkValidity();
+
+			if(valid){
+
+
+			var firstname   = $('#firstname').val();
+			var lastname    = $('#lastname').val();
+			var email       = $('#email').val();
+			var phonenumber = $('#phonenumber').val();
+			var password    = $('#password').val();
+
+				e.preventDefault();
+
+				$.ajax({
+					type: 'POST',
+					url: 'process.php',
+					data:{firstname: firstname,lastname: lastname,email: email,phonenumber: phonenumber,password: password},
+					success: function(data){
+						Swal.fire({
+						title: 'Successful!',
+						text: data,
+						type: 'success',
+						imageUrl: 'https://unsplash.it/400/200',
+						imageWidth: 400,
+						imageHeight: 200,
+						imageAlt: 'Custom image',
+							})
+					},
+					error: function(data){
+						Swal.fire({
+						title: 'Errors!',
+						text: 'There were errors while saving the data.',
+						type: 'error',
+						imageUrl: 'https://unsplash.it/400/200',
+						imageWidth: 400,
+						imageHeight: 200,
+						imageAlt: 'Custom image',
+					})
+					}
+				});
+				
+			}else{
+				
+			}
+
+		});
+
+	});
+
+</script>
+</body>
+</html>
